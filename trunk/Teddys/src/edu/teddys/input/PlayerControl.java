6c87cd3954fcb8a3ec40af7e788e76e737485af9
besient <besient@gmail.com>
2011-12-13 18:57:22 +0000

    neuer PlayerControl mit Physik

--- trunk/Teddys/src/edu/teddys/input/PlayerControl.java
+++ trunk/Teddys/src/edu/teddys/input/PlayerControl.java
@@ -1,40 +1,46 @@
 package edu.teddys.input;
 
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
 import com.jme3.input.InputManager;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 
 /**
  *
  * @author besient
  */
-public class PlayerControl implements AnalogListener {
+public class PlayerControl extends CharacterControl implements AnalogListener {
 
-    private float moveSpeed = 0.01f;
+    private float moveSpeed = 0.1f;
     
     private InputManager input;
-    private Node player;
     Vector3f vel;
     
-    public PlayerControl(Node player) {
-        this.player = player;
-    }
     
+    public PlayerControl(Spatial player, CollisionShape collisionShape, float stepHeight) {
+        super(collisionShape, stepHeight);
+        setPhysicsLocation(player.getWorldTranslation());
+        setFallSpeed(30);
+        setJumpSpeed(30);
+        setGravity(30);
+        player.addControl(this);        
+    }
+        
     public void registerWithInput(InputManager input) {
         this.input = input;
         
         input.addMapping("moveLeft", new KeyTrigger(KeyInput.KEY_A));
         input.addMapping("moveRight", new KeyTrigger(KeyInput.KEY_D));
-        input.addMapping("moveUp", new KeyTrigger(KeyInput.KEY_W));
-        input.addMapping("moveDown", new KeyTrigger(KeyInput.KEY_S));
+        input.addMapping("jump", new KeyTrigger(KeyInput.KEY_SPACE));
         
         input.addListener(this, new String[]{"moveLeft", 
-                                            "moveRight", 
-                                            "moveUp", 
-                                            "moveDown"});
+                                            "moveRight",
+                                            "jump"});
     }
     
     public void onAnalog(String name, float value, float tpf) {
@@ -42,19 +48,13 @@ public class PlayerControl implements AnalogListener {
         if (name.equals("moveLeft")) {
             vel = new Vector3f(-1, 0, 0);
             vel.multLocal(moveSpeed);
-            player.move(vel);
+            warp(getPhysicsLocation().add(vel));
         } else if (name.equals("moveRight")) {
             vel = new Vector3f(1, 0, 0);
             vel.multLocal(moveSpeed);
-            player.move(vel);
-        } else if (name.equals("moveUp")) {
-            vel = new Vector3f(0, 1, 0);
-            vel.multLocal(moveSpeed);
-            player.move(vel);
-        } else if (name.equals("moveDown")) {
-            vel = new Vector3f(0, -1, 0);
-            vel.multLocal(moveSpeed);
-            player.move(vel);
+            warp(getPhysicsLocation().add(vel));
+        } else if (name.equals("jump")) {
+            jump();
         }
 
     }
