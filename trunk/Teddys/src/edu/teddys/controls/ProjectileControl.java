3eb6517354e9067f19bd7f3073ba6b82b62678ec
Benjamin Siemoneit <benjamin@benjamin-F3Ka.(none)>
2012-01-11 21:33:06 +0100

    Vorbereitungen für erste Waffen

---
+++ trunk/Teddys/src/edu/teddys/controls/ProjectileControl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.teddys.controls;
+
+import com.jme3.bullet.control.RigidBodyControl;
+import edu.teddys.objects.weapons.Projectile;
+
+/**
+ *
+ * @author besient
+ */
+public class ProjectileControl extends RigidBodyControl {
+    
+    private Projectile projectile;
+    
+    public ProjectileControl(Projectile projectile) {
+        
+    }
+    
+}
---
+++ trunk/Teddys/src/edu/teddys/objects/weapons/Projectile.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.teddys.objects.weapons;
+
+import com.jme3.scene.Spatial;
+
+/**
+ *
+ * @author besient
+ */
+public class Projectile {
+    
+    private Spatial projectileVis;
+    
+    public Projectile() {
+        
+    }
+    
+    public void remove() {
+        
+    }
+}
