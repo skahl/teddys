e91598f864062d94d6c66e40ecc9d924c73ca687
Benjamin Siemoneit <benjamin@benjamin-F3Ka.(none)>
2012-01-12 10:56:50 +0100

    tpf in move integriert

--- trunk/Teddys/src/edu/teddys/input/PlayerControl.java
+++ trunk/Teddys/src/edu/teddys/input/PlayerControl.java
@@ -17,26 +17,25 @@ import edu.teddys.hud.HUDController;
  */
 public class PlayerControl extends CharacterControl implements AnalogListener, ActionListener {
 
-    private float moveSpeed = 0.02f;
-    private float totalEnergy, currentEnergy;
+    private float moveSpeed = 1f;
     private boolean jetpackActive;
+    private float jetpackEmptyRate = 75f;
+    private float jetpackRefreshRate = 25f;
+    private float totalJetpackEnergy = 100f;
+    private float currentEnergy = totalJetpackEnergy;
+    private float gravity = 4f;
     
     private InputManager input;
-    private  Vector3f vel;
+    private  Vector3f vel, left, right;
     
     
     public PlayerControl(Spatial player, CollisionShape collisionShape, float stepHeight) {
         super(collisionShape, stepHeight);
         setPhysicsLocation(player.getWorldTranslation());
-//        setFallSpeed(1f);
-//        setJumpSpeed(1f);
-//        setGravity(1f);
         player.addControl(this);   
         
-        
-        totalEnergy = 100;
-        currentEnergy = totalEnergy;
-
+        left = new Vector3f(-1,0,0);
+        right = new Vector3f(1,0,0);
     }
         
     public void registerWithInput(InputManager input) {
@@ -44,27 +43,21 @@ public class PlayerControl extends CharacterControl implements AnalogListener, A
         
         input.addMapping("moveLeft", new KeyTrigger(KeyInput.KEY_A));
         input.addMapping("moveRight", new KeyTrigger(KeyInput.KEY_D));
-        input.addMapping("jump", new KeyTrigger(KeyInput.KEY_SPACE));
-        input.addMapping("jetpack", new KeyTrigger(KeyInput.KEY_LCONTROL));
+        input.addMapping("jetpack", new KeyTrigger(KeyInput.KEY_SPACE));
         
         input.addListener(this, new String[]{"moveLeft", 
                                             "moveRight",
-                                            "jump",
                                             "jetpack"});
     }
     
     public void onAnalog(String name, float value, float tpf) {
         
         if (name.equals("moveLeft")) {
-            vel = new Vector3f(-1, 0, 0);
-            vel.multLocal(moveSpeed);
+            vel = left.mult(moveSpeed * tpf);
             warp(getPhysicsLocation().add(vel));
         } else if (name.equals("moveRight")) {
-            vel = new Vector3f(1, 0, 0);
-            vel.multLocal(moveSpeed);
+            vel = right.mult(moveSpeed * tpf);
             warp(getPhysicsLocation().add(vel));
-        } else if (name.equals("jump")) {
-            jump();
         }
 
     }
@@ -81,7 +74,7 @@ public class PlayerControl extends CharacterControl implements AnalogListener, A
     
     private void startJetpack() {
         if (currentEnergy > 0f) {
-            setGravity(-4f);
+            setGravity(-gravity);
             jetpackActive = true;
         } else {
             stopJetpack();
@@ -89,8 +82,14 @@ public class PlayerControl extends CharacterControl implements AnalogListener, A
     }
     
     private void stopJetpack() {
-        setGravity(4f);
+        setGravity(gravity);
         jetpackActive = false;
+    }
+    
+    @Override
+    public void setGravity(float value) {
+        super.setGravity(value);
+        gravity = value;
     }
     
     @Override
@@ -98,20 +97,17 @@ public class PlayerControl extends CharacterControl implements AnalogListener, A
         super.update(tpf);
         if (jetpackActive) {
             if (currentEnergy > 0) {
-                currentEnergy -= 75f * tpf;
+                currentEnergy -= jetpackEmptyRate * tpf;
             } else {
                 stopJetpack();
             }    
         } else {
-            if (currentEnergy < totalEnergy) {
-                currentEnergy += 25f * tpf;
+            if (currentEnergy < totalJetpackEnergy) {
+                currentEnergy += jetpackRefreshRate * tpf;
             }
         }
         
         HUDController.getInstance().setJetpackEnergy((int)currentEnergy);
     }
-
-    
-
     
 }
