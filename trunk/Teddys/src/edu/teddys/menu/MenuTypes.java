5bd492379c87c23de1f6e5ec797728289dc9bbd6
Benjamin Siemoneit <benjamin@benjamin-GA-880GA-UD3H.(none)>
2012-01-31 18:48:26 +0100

    MenuTypes versehentlich gelöscht

---
+++ trunk/Teddys/src/edu/teddys/menu/MenuTypes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.teddys.menu;
+
+/**
+ *
+ * @author besient
+ */
+public enum MenuTypes {
+    MAIN_MENU,
+    PAUSE_MENU,
+    OPTIONS_MENU,
+    JOIN_GAME,
+    CREATE_GAME,
+    CREDITS,
+    BLANK
+};
