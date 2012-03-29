baadeb99c8e11faa1a79664d2ddfa6410a0b5fec
Benjamin Siemoneit <benjamin@localhost.localdomain>
2012-03-29 10:59:16 +0200

    Scoretabelle

--- trunk/Teddys/src/edu/teddys/menu/PauseMenu.java
+++ trunk/Teddys/src/edu/teddys/menu/PauseMenu.java
@@ -7,7 +7,13 @@ package edu.teddys.menu;
 import com.jme3.app.Application;
 import com.jme3.input.InputManager;
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.elements.render.TextRenderer;
 import de.lessvoid.nifty.screen.Screen;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 /**
  * ScreenController for the pause menu
  * @author besient
@@ -20,6 +26,10 @@ public class PauseMenu extends MessagePopupController {
     
     private boolean enabled;
     
+    private final int numEntries = 5;
+    
+    private List<Element> ranks, names, kills, deaths;
+    private List<String> scores;
     
     /**
      * {@inheritDoc} 
@@ -27,6 +37,32 @@ public class PauseMenu extends MessagePopupController {
     @Override
     public void bind(Nifty nifty, Screen screen) {
         super.bind(nifty, screen);
+        
+        scores = new ArrayList<String>();
+        ranks = new ArrayList<Element>();
+        names = new ArrayList<Element>();
+        kills = new ArrayList<Element>();
+        deaths = new ArrayList<Element>();
+        for(int i = 0; i < numEntries; i++) {
+            ranks.add(nifty.getScreen("PAUSE_MENU").findElementByName("rank"+i));
+            names.add(nifty.getScreen("PAUSE_MENU").findElementByName("name"+i));
+            kills.add(nifty.getScreen("PAUSE_MENU").findElementByName("kill"+i));
+            deaths.add(nifty.getScreen("PAUSE_MENU").findElementByName("death"+i));
+        }
+    }
+    
+    private void updateSores() {
+        for (int i = 0; i < scores.size(); i++) {
+            String[] score = scores.get(i).split(":");
+            ranks.get(i).getRenderer(TextRenderer.class).setText(String.valueOf(i));
+            names.get(i).getRenderer(TextRenderer.class).setText(score[0]);
+            kills.get(i).getRenderer(TextRenderer.class).setText(score[1]);
+            deaths.get(i).getRenderer(TextRenderer.class).setText(score[2]);
+        }
+    }
+    
+    public void setScore(String score) {
+        
     }
 
 
